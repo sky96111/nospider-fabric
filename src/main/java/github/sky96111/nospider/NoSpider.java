d87e1c98150806071a909bb383b62abfe8511b61
sky96111 <sky96111@outlook.com>
2023-06-18 21:48:48 +0800

    refactor: use fabric-api

--- src/main/java/github/sky96111/nospider/NoSpider.java
+++ src/main/java/github/sky96111/nospider/NoSpider.java
@@ -1,11 +1,24 @@
 package github.sky96111.nospider;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NoSpider implements ModInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoSpider.class);
 
     @Override
     public void onInitialize() {
 
+        ScreenEvents.AFTER_INIT.register((client, screen, width, height) -> {
+            if (screen instanceof TitleScreen) {
+                LOGGER.info("TitleScreen init");
+            }
+        });
+
+        
     }
 }

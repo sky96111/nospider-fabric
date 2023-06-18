2d098d61c30f12b8a90419dc7f0d27375fb2b684
sky96111 <sky96111@outlook.com>
2023-06-18 19:48:31 +0800

    chore: use slf4j

--- src/main/java/github/sky96111/nospider/mixin/InitMixin.java
+++ src/main/java/github/sky96111/nospider/mixin/InitMixin.java
@@ -1,8 +1,9 @@
 package github.sky96111.nospider.mixin;
 
-import net.fabricmc.loader.impl.util.log.Log;
-import net.fabricmc.loader.impl.util.log.LogCategory;
+import github.sky96111.nospider.NoSpider;
 import net.minecraft.client.gui.screen.TitleScreen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class InitMixin {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoSpider.class);
+
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo ci) {
-        Log.info(LogCategory.LOG, "Init NoSpider Mixin");
+        LOGGER.info("Mixin init");
     }
 }

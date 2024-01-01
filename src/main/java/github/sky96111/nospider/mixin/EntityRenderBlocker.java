90fc0296c1d324d1f5dd206dc5e2a4b3ae3f3223
sky96111 <sky96111@outlook.com>
2024-01-01 21:55:27 +0800

    add: EntityRenderBlocker rename: EntityVisibleBlocker

--- src/main/java/github/sky96111/nospider/mixin/EntityRenderBlocker.java
+++ src/main/java/github/sky96111/nospider/mixin/EntityRenderBlocker.java
@@ -1,20 +1,24 @@
 package github.sky96111.nospider.mixin;
 
 import github.sky96111.nospider.NoSpider;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntityRenderer.class)
+@Mixin(WorldRenderer.class)
 public abstract class EntityRenderBlocker {
 
-    @Inject(at = @At("HEAD"), method = "isVisible", cancellable = true)
-    private void renderEntity(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(at = @At("HEAD"), method = "renderEntity", cancellable = true)
+    private void renderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta,
+                              MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+
         if (NoSpider.isBlockedType(entity.getType())) {
-            cir.setReturnValue(false);
+            ci.cancel();
         }
     }
 
---
+++ src/main/java/github/sky96111/nospider/mixin/EntityVisibleBlocker.java
@@ -0,0 +1,21 @@
+package github.sky96111.nospider.mixin;
+
+import github.sky96111.nospider.NoSpider;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class EntityVisibleBlocker {
+
+    @Inject(at = @At("HEAD"), method = "isVisible", cancellable = true)
+    private void renderEntity(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (NoSpider.isBlockedType(entity.getType())) {
+            cir.setReturnValue(false);
+        }
+    }
+
+}
